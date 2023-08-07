8d98941119b4e4597222cea1a3af6c1432cafbcc
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-07 15:55:40 +0400

    Added author model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=100, null=False, blank=False)
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return self.full_name()
+
 class Post(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
     excerpt = models.CharField(max_length=200, null=True, blank=True)
