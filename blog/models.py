a88d9ab88529c4789b52beb0f50ebbff51acbfac
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-09 14:00:32 +0400

    Registered models for admin

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,9 @@ class Author(models.Model):
 class Tag(models.Model):
     caption = models.CharField(max_length=20, null=True, blank=True)
 
+    def __str__(self):
+        return self.caption
+
 class Post(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
     excerpt = models.CharField(max_length=200, null=True, blank=True)
