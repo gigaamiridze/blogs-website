80ab267666c6e0c6b8a42348d0cd3ed677940f25
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-09 15:15:06 +0400

    Added prepopulated fields feature for post admin model

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,7 @@ from .models import *
 class PostAdmin(admin.ModelAdmin):
     list_filter = ('author', 'tags', 'date',)
     list_display = ('title', 'author', 'date',)
+    prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
